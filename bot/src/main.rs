cddf2fa50be960f73a08e116e282d4ab183a30c0
Iris <iris.devillars@gmail.com>
2023-06-22 12:00:46 +0200

    feat: check indexer status before starting renewing domains

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -14,6 +14,7 @@ use tokio::time::sleep;
 mod bot;
 mod config;
 mod discord;
+mod indexer_status;
 mod models;
 
 #[tokio::main]
@@ -50,53 +51,91 @@ async fn main() {
     let account =
         SingleOwnerAccount::new(provider, signer, conf.account.address, chain_id::TESTNET);
     println!("[bot] started");
+    let mut need_to_check_status = true;
     loop {
-        match bot::get_domains_ready_for_renewal(&conf, &shared_state).await {
-            Ok(domains) => {
-                println!("[indexer] checking domains to renew today");
-                if !domains.0.is_empty() && !domains.1.is_empty() {
-                    match renew_domains(&conf, &account, domains.clone()).await {
-                        Ok(_) => {
-                            match log_domains_renewed(&conf, domains).await {
-                                Ok(_) => {log_msg_and_send_to_discord(
+        if need_to_check_status {
+            match indexer_status::get_status_from_endpoint(&conf).await {
+                Ok(block) => {
+                    println!("Block: {}", block);
+                    match indexer_status::check_block_status(&conf, block).await {
+                        Ok(status) => {
+                            if status {
+                                need_to_check_status = false;
+                                log_msg_and_send_to_discord(
                                     &conf,
                                     "[bot][renewals]",
-                                    "All domains renewed successfully",
+                                    "Indexer is up to date, starting renewals",
                                 )
-                                .await}
-                                Err(error) => log_error_and_send_to_discord(&conf,"[bot][error] An error occurred while logging domains renewed into Discord",  &error).await
-                            };
+                                .await
+                            } else {
+                                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+                                continue;
+                            }
                         }
-                        Err(e) => {
-                            log_error_and_send_to_discord(
-                                &conf,
-                                "[bot][error] An error occurred while renewing domains",
-                                &e,
-                            )
-                            .await;
-                            break;
+                        Err(error) => {
+                            println!(
+                                "Error while checking block status: {}, retrying in 5 seconds",
+                                error
+                            );
+                            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
                         }
                     }
-                } else {
-                    log_msg_and_send_to_discord(
+                }
+                Err(error) => {
+                    println!(
+                        "Error getting indexer status, retrying in 5 seconds: {}",
+                        error
+                    );
+                    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+                }
+            }
+        } else {
+            match bot::get_domains_ready_for_renewal(&conf, &shared_state).await {
+                Ok(domains) => {
+                    println!("[indexer] checking domains to renew today");
+                    if !domains.0.is_empty() && !domains.1.is_empty() {
+                        match renew_domains(&conf, &account, domains.clone()).await {
+                            Ok(_) => {
+                                match log_domains_renewed(&conf, domains).await {
+                                    Ok(_) => {log_msg_and_send_to_discord(
+                                        &conf,
+                                        "[bot][renewals]",
+                                        "All domains renewed successfully",
+                                    )
+                                    .await}
+                                    Err(error) => log_error_and_send_to_discord(&conf,"[bot][error] An error occurred while logging domains renewed into Discord",  &error).await
+                                };
+                            }
+                            Err(e) => {
+                                log_error_and_send_to_discord(
+                                    &conf,
+                                    "[bot][error] An error occurred while renewing domains",
+                                    &e,
+                                )
+                                .await;
+                                break;
+                            }
+                        }
+                    } else {
+                        log_msg_and_send_to_discord(
+                            &conf,
+                            "[bot][renewals]",
+                            "No domains to renew today",
+                        )
+                        .await;
+                    }
+                }
+                Err(e) => {
+                    log_error_and_send_to_discord(
                         &conf,
-                        "[bot][renewals]",
-                        "No domains to renew today",
+                        "[bot][error] An error occurred while getting domains ready for renewal",
+                        &e,
                     )
                     .await;
                 }
             }
-            Err(e) => {
-                log_error_and_send_to_discord(
-                    &conf,
-                    "[bot][error] An error occurred while getting domains ready for renewal",
-                    &e,
-                )
-                .await;
-            }
+            // Sleep for 24 hours
+            sleep(std::time::Duration::from_secs(conf.renewals.delay)).await;
         }
-
-        // Sleep for 24 hours
-        sleep(std::time::Duration::from_secs(conf.renewals.delay)).await;
     }
 }
