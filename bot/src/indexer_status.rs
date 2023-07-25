e259651e013e751f156a48a5f607e3cca61e69a9
Thomas Marchand <thomas.marchand@tuta.io>
2023-07-25 12:05:00 +0200

    fix: connection with indexer

--- bot/src/indexer_status.rs
+++ bot/src/indexer_status.rs
@@ -6,15 +6,8 @@ use starknet::{
 
 use crate::{bot::get_provider, config::Config};
 
-pub async fn check_block_status(conf: &Config, block_nb: String) -> Result<bool> {
+pub async fn check_block_status(conf: &Config, block_nb: u64) -> Result<bool> {
     let provider = get_provider(&conf);
-    let block_nb: u64 = match block_nb.parse() {
-        Ok(block_nb) => block_nb,
-        Err(e) => {
-            println!("Unable to parse block number: {}", e);
-            return Ok(false);
-        }
-    };
 
     match provider.get_block(BlockId::Number(block_nb)).await {
         Ok(block) => {
@@ -38,13 +31,25 @@ pub async fn check_block_status(conf: &Config, block_nb: String) -> Result<bool>
     }
 }
 
-pub async fn get_status_from_endpoint(conf: &Config) -> Result<String> {
-    let response = reqwest::get(format!(
+pub async fn get_status_from_endpoint(conf: &Config) -> Result<u64, Box<dyn std::error::Error>> {
+    // Perform the request and get the response as a string
+    let raw_response = reqwest::get(format!(
         "{}:{}/is_ready",
         conf.indexer_server.server_url, conf.indexer_server.port
     ))
     .await?
-    .json()
+    .text()
     .await?;
-    Ok(response)
+
+    // Log the raw response
+    println!("Raw response: {}", &raw_response);
+
+    // Now, try to parse the raw response as JSON
+    let response: serde_json::Value = serde_json::from_str(&raw_response)?;
+
+    // Extract the "last_block" field as a usize
+    let last_block = response["last_block"].as_u64()
+        .ok_or("Failed to parse 'last_block' as usize")?;
+
+    Ok(last_block)
 }
